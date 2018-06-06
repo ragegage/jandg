dc881ebf4dd16ccbdf81e4c06a0949e851be78f8
Gage Newman <gagesnewman@gmail.com>
2018-06-05 22:17:30 -0700

    hack to set favicon once page loads

---
+++ js.js
@@ -0,0 +1,5 @@
+// set favicon once page loads
+document.addEventListener('DOMContentLoaded', function() {
+  const favicon = document.getElementsByTagName('link')[0]
+  favicon.href = 'http://gagenewman.com/jandg/heart.png'
+})
